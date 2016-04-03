d3ba3a9524d6f48b41dc1f3d5c0b552aaf9fe888
Abhimanyu Mongandh Ambalath <abhimanyu.m.a@gmail.com>
2016-04-04 04:41:39 +0530

    Intial configuration

--- Gemfile
+++ Gemfile
@@ -34,3 +34,13 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Custom Gems Start from here
+
+group :test do
+  gem "rspec-rails", "~> 2.14"
+  gem "factory_girl_rails"
+  gem 'ffaker'
+end
+
+gem 'pg'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+  namespace :api,
+    defaults: {format: :json},
+    constraints: {subdomain: 'api' },
+    path: '/' do
+
+    scope module: :v1 do
+
+    end
+
+  end
+
 end
