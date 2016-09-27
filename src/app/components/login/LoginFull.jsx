fb5dcb0668b90e30eba6eb107e646fa8ee66d513
Abhimanyu Mongandh Ambalath <abhimanyu.m.a@gmail.com>
2016-09-28 01:21:16 +0530

    Better looking login form

--- src/app/components/login/LoginFull.jsx
+++ src/app/components/login/LoginFull.jsx
@@ -3,46 +3,14 @@ import React from 'react';
 class LoginFull extends React.Component {
   render () {
     return (
-      <div className="login-form">
-        <form>
-          <ul className="flex-outer">
-            <li>
-              <label for="first-name">First Name</label>
-              <input type="text" id="first-name" placeholder="Enter your first name here" />
-            </li>
-            <li>
-              <label for="last-name">Last Name</label>
-              <input type="text" id="last-name" placeholder="Enter your last name here" />
-            </li>
-            <li>
-              <label for="email">Email</label>
-              <input type="email" id="email" placeholder="Enter your email here" />
-            </li>
-            <li>
-              <label for="phone">Phone</label>
-              <input type="tel" id="phone" placeholder="Enter your phone here" />
-            </li>
-            <li>
-              <label for="message">Message</label>
-              <textarea rows="6" id="message" placeholder="Enter your message here"></textarea>
-            </li>
-            <li>
-              <p>Age</p>
-              <ul className="flex-inner">
-                <li>
-                  <input type="checkbox" id="twenty-to-twentynine" />
-                  <label for="twenty-to-twentynine">20-29</label>
-                </li>
-                <li>
-                  <input type="checkbox" id="thirty-to-thirtynine" />
-                  <label for="thirty-to-thirtynine">30-39</label>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <button type="submit">Submit</button>
-            </li>
-          </ul>
+      <div className="login-form-container">
+        <form className="pure-form login-form">
+          <h3>Login</h3>
+          <fieldset className="pure-group input-fields">
+              <input type="text" className="pure-input-1" placeholder="Username" />
+              <input type="password" className="pure-input-1" placeholder="Password" />
+          </fieldset>
+          <button type="submit" className="pure-button pure-input-1-2 pure-button-primary">Sign in</button>
         </form>
       </div>
     );
