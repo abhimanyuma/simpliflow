f4a7f8ec83474e790bbad2fa19ee85b459954a83
Abhimanyu Mongandh Ambalath <abhimanyu.m.a@gmail.com>
2016-10-01 20:05:31 +0530

    Add even to login form

--- src/app/components/login/LoginFull.jsx
+++ src/app/components/login/LoginFull.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
 class LoginFull extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {username: "", password: ""};
+  }
+
+  handleLoginSubmit(event) {
+    event.preventDefault();
+    let username = this.refs.username.value;
+    let password = this.refs.password.value;
+    console.log(`Trying to log on with ${username} and ${password}`);
+    this.setState({
+      username: username,
+      password: password
+    });
+    this.refs.username.value = '';
+    this.refs.password.value = '';
+  }
+
   render () {
     return (
       <div className="login-form-container">
-        <form className="pure-form login-form">
+        <form className="pure-form login-form" onSubmit={this.handleLoginSubmit.bind(this)}>
           <h3>Login</h3>
           <fieldset className="pure-group input-fields">
-              <input type="text" className="pure-input-1" placeholder="Username" />
-              <input type="password" className="pure-input-1" placeholder="Password" />
+              <input type="text" className="pure-input-1" ref="username" placeholder="Username" />
+              <input type="password" className="pure-input-1" ref="password" placeholder="Password" />
           </fieldset>
-          <button type="submit" className="pure-button pure-input-1-2 pure-button-primary">Sign in</button>
+          <button type="submit" className="pure-button pure-input-1-2 pure-button-primary">Login</button>
         </form>
       </div>
     );
