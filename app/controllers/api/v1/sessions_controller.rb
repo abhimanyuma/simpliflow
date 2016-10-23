d1dbcf390a02f21d414d580502cf8d39dbfcdd0a
Abhimanyu Mongandh Ambalath <abhimanyu.m.a@gmail.com>
2016-10-23 15:48:19 +0530

    Enable basic login method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,14 +5,21 @@ class Api::V1::SessionsController < ApplicationController
   def create
 
     user_password = session_params[:password]
-    user_email = session_params[:email]
-    user = user_email.present? && User.find_by(email: user_email)
+    user_id = session_params[:user_id]
+    user = nil
+    if user_id.present?
+      user = User.find_by(email: user_id)
+      unless user
+        user = User.find_by(user_name: user_id)
+      end
+    end
 
     if user and user.valid_password? user_password
+
       sign_in user, store: false
       user.generate_authentication_token!
       user.save!
-      render json: user, status: 200, location: [:api, user]
+      render json: {status: true, data:user}, status: 200, location: [:api, user]
     else
       render json: { errors: "Invalid email or password" }, status: 422
     end
@@ -32,7 +39,7 @@ class Api::V1::SessionsController < ApplicationController
   private
 
     def session_params
-      params.require(:user).permit(:email, :password)
+      params.require(:user).permit(:password, :user_id)
     end
 
 end
