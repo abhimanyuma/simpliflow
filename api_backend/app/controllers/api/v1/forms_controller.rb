ec20d47675eba1b7b42bc36e9ea63d477d6a38a7
abhimanyuma <abhi@manyu.in>
2017-08-18 14:40:57 +0530

    Add edit option to edit form title and subtitle

--- api_backend/app/controllers/api/v1/forms_controller.rb
+++ api_backend/app/controllers/api/v1/forms_controller.rb
@@ -13,11 +13,39 @@ class Api::V1::FormsController < ApplicationController
   def show
 
     form = Form.find_by(uuid: params[:id])
-    if form.permissible_for_user?(current_user)
+    if form.blank?
+      render json: {status: false, errors: {"global": ["No such form"]}}, status: 404
+    elsif form.permissible_for_user?(current_user)
       render json: {status: true, data: form}, status: 200
     else
       render json: {status: false, errors: {"global": ["You are not authorized to view this form"]}}, status: 401
     end
+  end
+
+  def update
+    form = Form.find_by(uuid: params[:id])
+    if form.blank?
+      render json: {status: false, errors: {"global": ["No such form"]}}, status: 404
+    elsif form.permissible_for_user?(current_user)
+      form.assign_attributes(form_params)
+
+      if form.save!
+        render json: {status: true, data: form}
+      else
+        render json: {status: false, errors: form.errors}, status: 400
+      end
+    else
+      render json: {status: false, errors: {"global": ["You are not authorized to edit this form"]}}, status: 401
+    end
+
+  end
+
+  private
+
+  def form_params
+    params.require(:form).permit(
+      :title, :sub_title, :content, :content_type
+    )
   end
 
 
