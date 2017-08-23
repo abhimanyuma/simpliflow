888175a90c0f6230f63942c6998344049afb6bee
abhimanyuma <abhi@manyu.in>
2017-08-23 19:15:21 +0530

    Finally add a working bootstrap styled Trix toolbar

--- admin_console/src/app/components/custom/TrixEditor.jsx
+++ admin_console/src/app/components/custom/TrixEditor.jsx
@@ -3,41 +3,65 @@ import 'trix/dist/trix-core.js';
 
 import React from 'react';
 import { Link } from 'react-router';
+import LoadingFormContainer from '../common/LoadingFormContainer.jsx';
+
 
 
 class TrixEditor extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {}
+  }
+
+  componentWillMount() {
+     document.addEventListener("trix-initialize", (event) => {
+       console.log("This is ", this)
+       let trix_editor = document.querySelector("trix-editor").editor
+       this.setState({"trix_editor": trix_editor})
+     })
+
+     document.addEventListener("trix-change", (event) => {
+       console.log("Editor Change")
+     })
+  }
+
+  get_btn_class(attribute_name) {
+
+    if (!this.state.trix_editor) {
+      return "btn btn-secondary"
+    }
+
+    let inline_attributes = ["bold", "italic", "href", "strike"]
+    let active_class = "btn btn-outline-secondary"
+    let inactive_class = "btn btn-secondary"
+
+    if (inline_attributes.includes(attribute_name)) {
+      if (this.state.trix_editor.attributeIsActive(attribute_name)) {
+        return active_class
+      } else {
+        return inactive_class
+      }
+    }
   }
 
   render() {
+    console.log(this.props)
+    let display_class = "d-none"
+    if (this.state.trix_editor) {
+      display_class = ""
+      window.tred = this.state.trix_editor
+    }
     return (
       <div>
-        <div className="row reset-row-margin m4b d-flex justify-content-between">
-          <div className="btn-group mr-2" role="group" aria-label="First group">
-            <button type="button" className="btn btn-secondary"><i className="fa fa-bold" /></button>
-            <button type="button" className="btn btn-secondary"><i className="fa fa-italic"/></button>
-            <button type="button" className="btn btn-secondary"><i className="fa fa-strikethrough" /></button>
-            <button type="button" className="btn btn-secondary"><i className="fa fa-link"/></button>
+          <div>
+            {
+              /*Yes it is class and not className because this is a element that will
+            be modified by trix-core.js */
+            }
+            <trix-toolbar id="custom-trix-toolbar-instance" class="custom-trix-toolbar m4b"></trix-toolbar>
+            <trix-editor toolbar="custom-trix-toolbar-instance"></trix-editor>
           </div>
-          <div className="btn-group mr-2" role="group" aria-label="Second group">
-            <button type="button" className="btn btn-secondary"><i className="fa fa-header"/></button>
-            <button type="button" className="btn btn-secondary"><i className="fa fa-quote-right"/></button>
-            <button type="button" className="btn btn-secondary"><i className="fa fa-code"/></button>
-            <button type="button" className="btn btn-secondary"><i className="fa fa-list-ul"/></button>
-            <button type="button" className="btn btn-secondary"><i className="fa fa-list-ol"/></button>
-            <button type="button" className="btn btn-secondary"><i className="fa fa-outdent"/></button>
-            <button type="button" className="btn btn-secondary"><i className="fa fa-indent"/></button>
-          </div>
-          <div className="btn-group" role="group" aria-label="Third group">
-            <button type="button" className="btn btn-secondary"><i className="fa fa-undo"/></button>
-            <button type="button" className="btn btn-secondary"><i className="fa fa-rotate-right"/></button>
-          </div>
-        </div>
-        <div>
-          <trix-editor></trix-editor>
-        </div>
       </div>
     )
   }
