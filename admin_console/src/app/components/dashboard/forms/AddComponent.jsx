1bee1d1d9501a2df4adbeb6730a569ae9694f856
abhimanyuma <abhi@manyu.in>
2017-09-10 14:13:55 +0530

    Add a dummy add component table

--- admin_console/src/app/components/dashboard/forms/AddComponent.jsx
+++ admin_console/src/app/components/dashboard/forms/AddComponent.jsx
@@ -5,20 +5,70 @@ import { Link } from 'react-router';
 
 import LoadingFormContainer from '../../common/LoadingFormContainer.jsx';
 import MainFormContainer from '../../form/MainFormContainer.jsx'
+import ContentComponentsList from './component_lists/ContentComponentsList.jsx'
+import FormComponentsList from './component_lists/FormComponentsList.jsx'
+import SmartComponentsList from './component_lists/SmartComponentsList.jsx'
+import MediaFileComponentsList from './component_lists/MediaFileComponentsList.jsx'
+import LogicalComponentsList from './component_lists/LogicalComponentsList.jsx'
 
 import * as URL from '../../../common/url.js';
+import { generateUnsafeUniqueId  } from '../../../common/common.js';
 
 
 class AddComponent extends React.Component {
 
   constructor(props) {
     super(props);
-
+    this.component_id = generateUnsafeUniqueId(12)
   }
+
+  get_component_list() {
+    return {
+      "content" : {
+        "text" : "Content",
+        "component" : <ContentComponentsList />
+      },
+      "form" : {
+        "text" : "Form",
+        "component" : <FormComponentsList />
+      },
+      "media" : {
+        "text" : "Media & Files",
+        "component" : <MediaFileComponentsList />
+      },
+      "smart" : {
+        "text" : "Smart",
+        "component" : <SmartComponentsList />
+      },
+      "logical" : {
+        "text" : "Logical",
+        "component" : <LogicalComponentsList />
+      }
+    }
+  }
+
   render() {
+    const component_list = this.get_component_list()
+    const prefix = this.component_id
+    const tabItems = Object.keys(component_list).map((component_kind, index ) => {
+      return ( <li className="nav-item" key={`${prefix}-${component_kind}-tab-li`}>
+            <a className="nav-link" id={`${prefix}-${component_kind}-tab`}  data-toggle="pill" href={`#${prefix}-${component_kind}`} role="tab" aria-controls={`${prefix}-${component_kind}`} aria-expanded="true">{component_list[component_kind]["text"]}</a>
+          </li>)
+    })
+    const tabs = Object.keys(component_list).map((component_kind, index ) => {
+      return ( <div className="tab-pane fade" id={`${prefix}-${component_kind}`} role="tabpanel" aria-labelledby={`${prefix}-${component_kind}-tab`}>
+      {component_list[component_kind]["component"]}
+      </div>)
+    })
     return(
       <div className="card-body">
-          Ability to add content comes here
+
+        <ul className="nav nav-pills mb-3" id={`${prefix}-component`} role="tablist">
+          {tabItems}
+        </ul>
+        <div className="tab-content" id="pills-tabContent">
+          {tabs}
+        </div>
       </div>
     );
   }
---
+++ admin_console/src/app/components/dashboard/forms/component_lists/ContentComponentsList.jsx
@@ -0,0 +1,45 @@
+// @flow
+
+import React from 'react';
+import { Link } from 'react-router';
+
+class ContentComponentsList extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
+
+
+  render() {
+    return(
+      <div>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Component</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Content Box</td>
+              <td>Add HTML Markdown content. Supports templating.</td>
+            </tr>
+            <tr>
+              <td>Video Element</td>
+              <td>Add video element from YouTube/Facebook</td>
+            </tr>
+            <tr>
+              <td>Facebook/Twitter Embed</td>
+              <td>Embed various elements from Facebook/Twitter </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default ContentComponentsList;
